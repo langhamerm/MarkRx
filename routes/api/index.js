551728b18758a99a2c6adb21e7deb99a993a91a7
Mark Langhamer <langhamerm@gmail.com>
2019-05-01 03:44:47 -0500

    cleaning

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const doctorRoutes = require("./doctors");
 const patientRoutes = require("./patients");
-const schedulerRoutes = require("./scheduler");
 
 // doctor routes
 router.use("/doctors", doctorRoutes);
@@ -9,9 +8,5 @@ router.use("/patients", patientRoutes);
 
 // patient routes
 router.use("/patients", patientRoutes);
-
-//scheduler routes
-router.use("/scheduler", schedulerRoutes);
-
 
 module.exports = router;
